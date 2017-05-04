0ecd70c6917a562d554a253b9ed11ab0c4f35a2f
monkey92 <2223445830@qq.com>
2017-05-04 15:15:16 +0800

    updae

--- php/time.php
+++ php/time.php
@@ -2,7 +2,7 @@
 
 //获得当前月的最后一天
 function get_last_day_of_month(){
-	return date('Y-m-d 24:59:59',strtotime(date('Y-m-01',time()).' +1 month -1 day'));	
+	return date('Y-m-d 23:59:59',strtotime(date('Y-m-01',time()).' +1 month -1 day'));	
 }
 
 //获得当前毫秒值
@@ -11,3 +11,5 @@ function msectime() {
     $msectime =  sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
     return $msectime;
 }
+
+

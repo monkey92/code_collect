c7007e5f8a2dc1405047e8fcdb5be17aac71a826
monkey92 <2223445830@qq.com>
2017-05-04 16:37:25 +0800

    update

--- php/time.php
+++ php/time.php
@@ -12,4 +12,39 @@ function msectime() {
     return $msectime;
 }
 
+//时间显示处理
+public static function timeStr($time_str){
+
+    $time = strtotime($time_str);
+    $cur_time = time();
+    $m_10 = 10 * 60;
+    $h_1 =  1 * 60 * 60;
+    $h_12 = 12 * 60 * 60;
+    $h_24 = 24 * 60 * 60;
+    $h_48 = 48 * 60 * 60;
+    $h_72 = 72 * 60 * 60;
+
+    $expend = $cur_time-$time;
+
+    switch($expend){
+        case $expend < $m_10 :
+            $str = '刚刚';
+            break;
+        case $expend >= $m_10 && $expend < $h_1 :
+            $str = floor($expend/60).'分钟前';
+            break;
+        case $expend >= $h_1 && $expend < $h_24 :
+            $str = floor($expend/3600).'小时前';
+            break;
+        case $expend >= $h_24 && $expend < $h_72:
+            $str = floor($expend / (24*3600)).'天前';
+            break;
+        default:
+            $str = date("m/d",$time);
+    }
+    return $str;
+}
+
+
+
 
